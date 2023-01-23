96d57df903fb450aca781cfeffcbe285b7e852a9
J1min <aoiharu41@gmail.com>
2023-01-24 05:52:55 +0900

    Feat: add titles

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,15 +3,21 @@ import type { NextPage } from "next";
 import MainLayout from "@/layout/App";
 import Utterance from "@/components/Utterance";
 import Title from "@/components/Title";
+import Head from "next/head";
 
 const Main: NextPage = () => {
   return (
-    <section id={`main`}>
-      <MainLayout
-        utterance={<Utterance repo="J1min/comments" theme="photon-dark" />}
-        title={<Title>한마디 해주기</Title>}
-      />
-    </section>
+    <>
+      <Head>
+        <title>한마디 해주기</title>
+      </Head>
+      <section id={`main`}>
+        <MainLayout
+          utterance={<Utterance repo="J1min/comments" theme="photon-dark" />}
+          title={<Title>한마디 해주기 🤔</Title>}
+        />
+      </section>
+    </>
   );
 };
 
